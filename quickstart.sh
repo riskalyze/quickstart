7e69462558062beecc195f83c066c59864d3a08b
Jesse Bye <jbye@riskalyze.com>
2021-06-25 15:53:03 -0700

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -10,24 +10,26 @@ white=$'\e[0m'
 echo "👋 Welcome! This script takes care of first-time setup of your computer."
 echo "🏁 First, let's make sure Homebrew is installed."
 
-if ! command -v brew; then
+if ! command -v brew &>/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 echo "🍺 Great! Now, let's set up your GitHub account."
 
-if ! command -v gh; then
+if ! command -v gh &>/dev/null; then
   brew install gh
 fi
 
-if ! gh auth status 2>/dev/null; then 
+if ! gh auth status &>/dev/null; then 
   gh auth login -w
 fi
 
 echo "🙌 Wonderful! GitHub is all set."
 echo "🪄 Next, let's install Cast (Riskalyze's multi-purpose dev tool)."
 
-brew tap riskalyze/homebrew-taps
-brew install cast
+if ! command -v cast &>/dev/null; then
+  brew tap riskalyze/homebrew-taps
+  brew install cast
+fi
 
 echo "✨ Success! You are now ready to finish setting things up. Please run `cast system install` to continue."
