f1794ce10b5a9bd3d11522b23f8c07855c342ce4
Jesse Bye <jbye@riskalyze.com>
2021-06-28 10:57:16 -0700

    simplify readme

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Start here to configure your laptop for development at Riskalyze!
 
 ## Instructions
 
-1. Open Terminal.
-2. Paste this and hit enter: `curl -s https://raw.githubusercontent.com/riskalyze/quickstart/main/quickstart.sh | bash`
-3. Follow the prompts.
+Paste this in a terminal and hit enter:
+
+```shell
+curl -s https://raw.githubusercontent.com/riskalyze/quickstart/main/quickstart.sh | bash
+```
--- quickstart.sh
+++ quickstart.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+ARCH=$(uname -m | sed 's/x86_64/amd64/')
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+
 echo "👋 Welcome! This script takes care of first-time setup of your computer."
 echo "🏁 First, let's make sure Homebrew is installed."
 
@@ -9,11 +12,12 @@ fi
 
 echo "🍺 Great! Now, let's set up your GitHub account."
 
-if ! command -v gh &>/dev/null; then
-  brew install gh
-fi
+brew bundle --no-upgrade --file=- <<-EOF
+brew "fetch"
+brew "gh"
+EOF
 
-if ! gh auth status &>/dev/null; then 
+if ! gh auth status &>/dev/null; then
   gh auth login -w
 fi
 
@@ -21,8 +25,10 @@ echo "🙌 Wonderful! GitHub is all set."
 echo "🪄 Next, let's install Cast (Riskalyze's multi-purpose dev tool)."
 
 if ! command -v cast &>/dev/null; then
-  brew tap riskalyze/cast git@github.com:riskalyze/cast
-  brew install cast
+  GITHUB_OAUTH_TOKEN=$(gh auth status -t 2> >(grep -oh 'gho.*'))
+  CAST_TARBALL="cast_${OS}_${ARCH}.tar.gz"
+  fetch --repo https://github.com/riskalyze/cast --tag "~>1.0" --release-asset="${CAST_TARBALL}" --github-oauth-token "${GITHUB_OAUTH_TOKEN}" /tmp
+  tar -xzvf /tmp/"${CAST_TARBALL}" -C /usr/local/bin cast
 fi
 
-echo "✨ Success! You are now ready to finish setting things up. Please run `cast system install` to continue."
+echo "✨ Success! You are now ready to finish setting things up. Please run $(cast system install) to continue."
